b6d5fe2b9bebaa731e2f73a9f7da9dfac8b7eff3
chesnokov182-source <chesnokov182@gmail.com>
2026-04-20 09:11:31 +0500

    Create n2.js

---
+++ n2.js
@@ -0,0 +1,15 @@
+// Кроссворды уровня N2
+window.crosswordsData = window.crosswordsData || {};
+window.crosswordsData.n2 = {
+    name: "JLPT N2",
+    puzzles: [
+        {
+            name: "Кроссворд 1",
+            width: 10,
+            height: 10,
+            price: 0,
+            words: [
+            ]
+        }
+    ]
+};
