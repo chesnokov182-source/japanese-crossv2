68b241a2298bb717cdf4ca175bf77b8c9b6ef32c
chesnokov182-source <chesnokov182@gmail.com>
2026-04-20 09:11:16 +0500

    Create n1.js

---
+++ n1.js
@@ -0,0 +1,15 @@
+// Кроссворды уровня N1
+window.crosswordsData = window.crosswordsData || {};
+window.crosswordsData.n1 = {
+    name: "JLPT N1",
+    puzzles: [
+        {
+            name: "Кроссворд 1",
+            width: 12,
+            height: 12,
+            price: 0,
+            words: [
+            ]
+        }
+    ]
+};
