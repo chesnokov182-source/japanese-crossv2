37563741b995872501b8bab708b5137e88d6f8b5
chesnokov182-source <chesnokov182@gmail.com>
2026-04-20 09:12:18 +0500

    Create n5.js

---
+++ n5.js
@@ -0,0 +1,148 @@
+// Кроссворды уровня N5
+window.crosswordsData = window.crosswordsData || {};
+window.crosswordsData.n5 = {
+        name: "JLPT N5",
+        puzzles: [
+            {
+                name: "Кроссворд 1",
+                width: 8,
+                height: 6,
+                price: 0,
+                words: [
+                    { word: "ニホンゴ", row: 3, col: 0, dir: "across", clue: "Японский язык", number: 1 },
+                    { word: "ウンテンシユ", row: 0, col: 2, dir: "down", clue: "Водитель", number: 2},
+                    { word: "デンワバンゴウ", row: 1, col: 1, dir: "across", clue: "Номер телефона", number: 3},
+                    { word: "ギンコウ", row: 0, col: 5, dir: "down", clue: "Банк", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 2",
+                width: 7,
+                height: 4,
+                price: 50,
+                words: [
+                    { word: "イソガシイ", row: 1, col: 0, dir: "across", clue: "Занятой", number: 1},
+                    { word: "オシエル", row: 0, col: 3, dir: "down", clue: "Учить, рассказывать", number: 2},
+                    { word: "オハヨウ", row: 0, col: 3, dir: "across", clue: "Доброе утро", number: 2.1},
+                    { word: "メイシ", row: 0, col: 0, dir: "down", clue: "Визитка", number: 3},
+                    { word: "ハイ", row: 0, col: 4, dir: "down", clue: "Да", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 3",
+                width: 6,
+                height: 6,
+                price: 50,
+                words: [
+                    { word: "マイシユウ", row: 2, col: 1, dir: "across", clue: "Каждую неделю", number: 1},
+                    { word: "キノウ", row: 4, col: 0, dir: "across", clue: "День перед сегодня", number: 2},
+                    { word: "スイヨウビ", row: 1, col: 2, dir: "down", clue: "3-й день начиная с понедельника", number: 3},
+                    { word: "コトシ", row: 0, col: 3, dir: "down", clue: "Текущий год", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 4",
+                width: 8,
+                height: 6,
+                price: 50,
+                words: [
+                    { word: "コンシユウ", row: 2, col: 3, dir: "across", clue: "Текущая неделя", number: 1},
+                    { word: "キヨネン", row: 5, col: 0, dir: "across", clue: "Прошлый год", number: 2},
+                    { word: "コンバン", row: 2, col: 3, dir: "down", clue: "Сегодняшний вечер", number: 1.2},
+                    { word: "アシタ", row: 1, col: 5, dir: "down", clue: "Завтрашний день", number: 3},
+                    { word: "ドヨウビ", row: 0, col: 7, dir: "down", clue: "6-й день начиная с понедельника", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 5",
+                width: 7,
+                height: 6,
+                price: 50,
+                words: [
+                    { word: "ハタラキマス", row: 1, col: 1, dir: "across", clue: "Работать", number: 1},
+                    { word: "ハナシマス", row: 1, col: 1, dir: "down", clue: "Говорить, рассказывать", number: 1.1},
+                    { word: "オシエマス", row: 3, col: 0, dir: "across", clue: "Обучать, объяснять", number: 2},
+                    { word: "イキマス", row: 0, col: 4, dir: "down", clue: "Идти, ходить", number: 3},
+                    { word: "スワリマス", row: 1, col: 6, dir: "down", clue: "Сидеть", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 6",
+                width: 8,
+                height: 6,
+                price: 100,
+                words: [
+                    { word: "ベンキヨウシマス", row: 1, col: 0, dir: "across", clue: "Учиться", number: 1},
+                    { word: "ナキマス", row: 5, col: 1, dir: "across", clue: "Плакать", number: 2},
+                    { word: "カキマス", row: 0, col: 2, dir: "down", clue: "Писать", number: 3},
+                    { word: "ウタイマス", row: 1, col: 4, dir: "down", clue: "Петь", number: 4},
+                    { word: "ミマス", row: 0, col: 6, dir: "down", clue: "Смотреть", number: 5},
+                ]
+            },
+            {
+                name: "Кроссворд 7",
+                width: 8,
+                height: 7,
+                price: 100,
+                words: [
+                    { word: "ケシマス", row: 0, col: 1, dir: "across", clue: "Тушить, гасить; Выключать", number: 1},
+                    { word: "ケツコンシマス", row: 0, col: 1, dir: "down", clue: "Жениться, Выходить замуж", number: 1.1},
+                    { word: "レンシユウシマス", row: 3, col: 0, dir: "across", clue: "Тренироваться, обучаться", number: 2},
+                    { word: "キマス", row: 5, col: 3, dir: "across", clue: "Приходить, приезжать", number: 3},
+                    { word: "カシマス", row: 2, col: 5, dir: "down", clue: "Давать в долг", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 8",
+                width: 6,
+                height: 6,
+                price: 100,
+                words: [
+                    { word: "ツカイマス", row: 0, col: 1, dir: "across", clue: "Использовать", number: 1},
+                    { word: "オコリマス", row: 2, col: 0, dir: "across", clue: "Злиться", number: 2.1},
+                    { word: "オキマス", row: 2, col: 0, dir: "down", clue: "Просыпаться", number: 2},
+                    { word: "カエリマス", row: 0, col: 2, dir: "down", clue: "Возвращаться", number: 3},
+                    { word: "スミマス", row: 2, col: 4, dir: "down", clue: "Жить, проживать", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 9",
+                width: 7,
+                height: 4,
+                price: 100,
+                words: [
+                    { word: "エンピツ", row: 2, col: 0, dir: "across", clue: "Используется для письма. Написанное можно стереть", number: 1},
+                    { word: "ガクセイ", row: 1, col: 3, dir: "across", clue: "Учится в школе", number: 2},
+                    { word: "コウエン", row: 0, col: 0, dir: "down", clue: "Кто угодно может в нем играть. Находится на улице", number: 3},
+                    { word: "ゴガツ", row: 0, col: 3, dir: "down", clue: "5-й месяц", number: 4},
+                    { word: "イチゴ", row: 1, col: 6, dir: "down", clue: "Маленькая красная ягода. Часто используется в тортах", number: 5},
+                ]
+            },
+            {
+                name: "Кроссворд 10",
+                width: 8,
+                height: 5,
+                price: 100,
+                words: [
+                    { word: "アイスクリーム", row: 0, col: 1, dir: "across", clue: "Холодная еда из смеси молока и яиц", number: 1},
+                    { word: "パン", row: 4, col: 0, dir: "across", clue: "Выпекаемое мучное изделие", number: 2},
+                    { word: "トリニク", row: 3, col: 4, dir: "across", clue: "Мясо птицы", number: 3},
+                    { word: "アサゴハン", row: 0, col: 1, dir: "down", clue: "Рис на завтрак", number: 1.1},
+                    { word: "リヨウリ", row: 0, col: 5, dir: "down", clue: "Готовить еду", number: 4},
+                ]
+            },
+            {
+                name: "Кроссворд 11",
+                width: 6,
+                height: 8,
+                price: 100,
+                words: [
+                    { word: "ヒルゴハン", row: 2, col: 0, dir: "across", clue: "Рис на обед", number: 1},
+                    { word: "バナナ", row: 4, col: 3, dir: "across", clue: "Длинный продолговатый фрукт", number: 2},
+                    { word: "ラーメン", row: 7, col: 2, dir: "across", clue: "Китайская лапша. Очень популярна в Японии", number: 3},
+                    { word: "ハンバーガー", row: 2, col: 3, dir: "down", clue: "Блюдо из котлеты между булками", number: 4},
+                    { word: "ウドン", row: 0, col: 4, dir: "down", clue: "Длинная продолговатая белая лапша изготовляемая из пшеничной муки", number: 5},
+                ]
+            }
+          ]
+    };
