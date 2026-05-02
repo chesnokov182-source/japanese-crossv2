431163c475bcec4b7990036f58f964e04dbe54bb
chesnokov182-source <chesnokov182@gmail.com>
2026-05-02 16:52:34 +0500

    Update dailyTasks.js

--- js/dailyTasks.js
+++ js/dailyTasks.js
@@ -8,7 +8,7 @@ const TASKS_LIST = [
     { id: 'earn_100_points', name: 'Заработать 200 очков', target: 200, progress: 0, reward: 20 },
     { id: 'use_hint', name: 'Использовать 2 подсказки', target: 2, progress: 0, reward: 20 },
     { id: 'buy_skin', name: 'Купить скин', target: 1, progress: 0, reward: 20 },
-    { id: 'win_roulette', name: 'Выиграть в рулетке 200 очков', target: 200, progress: 0, reward: 60 },
+    { id: 'win_roulette', name: 'Выиграть в рулетке 200 очков', target: 200, progress: 0, reward: 20 },
     { id: 'complete_any_word', name: 'Угадать любое слово', target: 1, progress: 0, reward: 20 },
     { id: 'spend_200_points', name: 'Потратить 200 очков', target: 200, progress: 0, reward: 20 },
     { id: 'play_roulette_5', name: 'Сыграть в рулетку 5 раз', target: 5, progress: 0, reward: 20 },
