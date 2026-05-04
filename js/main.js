b97c2cf3991d42161d671705485af9967a4ab1fc
chesnokov182-source <chesnokov182@gmail.com>
2026-05-04 12:52:51 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ import {
     updatePuzzleSelect, resetCrossword, buyCurrentPuzzle, giveHint, isPuzzleUnlocked
 } from './crossword.js';
 import { loadDailyTasks, renderDailyTasksPanel } from './dailyTasks.js';
-import { loadThemesData, getAvailableThemes, applyTheme, purchaseTheme, renderThemesList } from './themes.js';
+import { loadThemesData, getAvailableThemes, applyTheme, purchaseTheme, renderThemesList, currentThemeId, purchasedThemes } from './themes.js';
 
 function applySavedTheme() {
     const savedTheme = localStorage.getItem('theme') || 'light';
